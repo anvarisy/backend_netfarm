e6d485cbbd2e4fef5fc23553138d41ac47e6aff0
Andri nova riswanto <anvarisy@gmail.com>
2020-10-06 22:38:01 +0700

    Insya Allah

--- backend_netfarm/settings.py
+++ backend_netfarm/settings.py
@@ -148,13 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/root/'
+STATIC_URL = '/backend_netfarm/root/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),)
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-STATIC_ROOT = '/home/dafqid/backend_netfarm/root/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+# STATIC_ROOT = '/home/dafqid/backend_netfarm/root/'
 
-MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/home/dafqid/backend_netfarm/media/'
+MEDIA_URL = '/backend_netfarm/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = '/home/dafqid/backend_netfarm/media/'
